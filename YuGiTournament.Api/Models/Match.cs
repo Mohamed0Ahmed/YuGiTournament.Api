831831090ee09a1ae7c404bc607d8fe30666762d
Mohamed Ahmed <mahmmaad96@gmail.com>
2025-03-23 01:49:50 +0200

    Add Models

---
+++ YuGiTournament.Api/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿namespace YuGiTournament.Api.Models
+{
+    public class Match
+    {
+        public int MatchId { get; set; }  
+        public int Player1Id { get; set; }  
+        public int Player2Id { get; set; }  
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Player Player1 { get; set; } = null!;
+        public Player Player2 { get; set; } = null!;
+    }
+}
---
+++ YuGiTournament.Api/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace YuGiTournament.Api.Models
+{
+    public class Player
+    {
+
+        public int PlayerId { get; set; }  
+        public string FullName { get; set; } = string.Empty;
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Points { get; set; }
+    }
+}
