ca020b6fe68bff6892f90904c493db45a09d1c2c
Mohamed Ahmed <mahmmaad96@gmail.com>
2025-07-27 12:37:53 +0300

    Add WinnerId To Match

---
+++ YuGiTournament.Api/Migrations/20250727085227_AddWinnerIdToMatch.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YuGiTournament.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWinnerIdToMatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Stage",
+                table: "Matches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WinnerId",
+                table: "Matches",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WinnerId",
+                table: "Matches");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Stage",
+                table: "Matches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+        }
+    }
+}
